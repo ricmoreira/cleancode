2f84c0948d9ed30de4d03352aa89df87e5f1c51b
pedro.morais2@vodafone.com <pedro.morais2@vodafone.com>
2022-02-02 19:07:43 +0000

    grupo8

--- src/main/java/staticanalysis/Case8.java
+++ src/main/java/staticanalysis/Case8.java
@@ -1,9 +1,15 @@
 package staticanalysis;
 
 public class Case8 {
-    private int a, b;
 
-    public void method(){
-        int c; int d;
+    // variáveis com nome pouco claro
+    private int numberA, numberB;
+
+    public void initialValue() {
+        // variáveis não estão a ser utilizadas
+        // mudança de nome das variáveis c e d para a e b
+        this.numberA = 0;
+        this.numberB = 0;
+
     }
 }
