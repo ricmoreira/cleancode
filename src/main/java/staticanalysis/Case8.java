5d926fa1fe6d4c3f7683bbbb15c85c6931ef3e01
ricfigue21 <92948572+ricfigue21@users.noreply.github.com>
2022-02-02 18:56:54 +0000

    Update Case8.java

--- src/main/java/staticanalysis/Case8.java
+++ src/main/java/staticanalysis/Case8.java
@@ -1,9 +1,13 @@
 package staticanalysis;
 
 public class Case8 {
-    private int a, b;
+    // variáveis com nome pouco claro
+    private int numberA, numberB;
 
-    public void method(){
-        int c; int d;
+    public void initialValue() {
+        // variáveis não estão a ser utilizadas
+        // mudança de nome das variáveis c e d para a e b
+        this.numberA = 0;
+        this.numberB = 0;
     }
 }
